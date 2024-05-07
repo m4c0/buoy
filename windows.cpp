45da0ed7c916cbcde181a20c4ca690ee40503839
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-07 14:16:46 +0100

    windows: making dir, etc

--- windows.cpp
+++ windows.cpp
@@ -13,8 +13,8 @@ namespace {
 struct deleter {
   void operator()(PWSTR p) { CoTaskMemFree(p); }
 };
-using pwstr = hai::value_holder<PWSTR, deleter>;
 
+using pwstr = hai::value_holder<PWSTR, deleter>;
 mno::req<pwstr> get_sg_folder() {
   pwstr res{};
   if (S_OK ==
@@ -24,32 +24,39 @@ mno::req<pwstr> get_sg_folder() {
   return mno::req<pwstr>::failed("Could not find Saved Games folder");
 }
 
-mno::req<void> find_file(const pwstr &sgfld, jute::view folder,
-                         jute::view file) {
+using wcb = hai::array<wchar_t>;
+mno::req<wcb> find_folder(const pwstr &sgfld, jute::view folder) {
   size_t count{};
 
   wchar_t wfld[MAX_PATH + 1];
   mbstowcs_s(&count, wfld, folder.size() + 1, folder.data(), _TRUNCATE);
   wfld[count + 1] = 0;
 
+  wcb buffer{MAX_PATH};
+  wcscpy_s(buffer.begin(), MAX_PATH, *sgfld);
+  wcscat_s(buffer.begin(), MAX_PATH, L"\\");
+  wcscat_s(buffer.begin(), MAX_PATH, wfld);
+  return mno::req<wcb>{traits::move(buffer)};
+}
+
+mno::req<wcb> mkdir(wcb dir) {
+  auto res = _wmkdir(dir.begin());
+  if (res != 0 && errno != EEXIST) {
+    return mno::req<wcb>::failed("Could not create folder in 'Saved Games'");
+  }
+  return mno::req<wcb>{traits::move(dir)};
+}
+
+mno::req<void> find_file(wchar_t *buffer, jute::view file) {
+  size_t count{};
+
   wchar_t wfn[MAX_PATH + 1];
   mbstowcs_s(&count, wfn, file.size() + 1, file.data(), _TRUNCATE);
   wfn[count + 1] = 0;
 
-  wchar_t buffer[MAX_PATH];
-  wcscpy_s(buffer, MAX_PATH, *sgfld);
-  wcscat_s(buffer, MAX_PATH, L"\\");
-  wcscat_s(buffer, MAX_PATH, wfld);
-
-  auto res = _wmkdir(buffer);
-  if (res != 0 && errno != EEXIST) {
-    return mno::req<void>::failed("Could not create folder in 'Saved Games'");
-  }
-
   wcscat_s(buffer, MAX_PATH, L"\\");
   wcscat_s(buffer, MAX_PATH, wfn);
 
-  _putws(buffer);
   //_wfopen_s(&f, name, L"rb");
   return {};
 }
@@ -58,12 +65,15 @@ mno::req<void> find_file(const pwstr &sgfld, jute::view folder,
 mno::req<hai::uptr<yoyo::reader>> buoy::open_for_reading(jute::view folder,
                                                          jute::view file) {
   return get_sg_folder()
-      .fmap([&](auto &&sg) { return find_file(sg, folder, file); })
+      .fmap([&](auto &&sg) { return find_folder(sg, folder); })
+      .fmap([&](auto &&dir) { return find_file(dir.begin(), file); })
       .fmap([] { return mno::req<hai::uptr<yoyo::reader>>::failed("TBD"); });
 }
 mno::req<hai::uptr<yoyo::writer>> buoy::open_for_writing(jute::view folder,
                                                          jute::view file) {
   return get_sg_folder()
-      .fmap([&](auto &&sg) { return find_file(sg, folder, file); })
+      .fmap([&](auto &&sg) { return find_folder(sg, folder); })
+      .fmap([&](auto &&dir) { return mkdir(traits::move(dir)); })
+      .fmap([&](auto &&dir) { return find_file(dir.begin(), file); })
       .fmap([] { return mno::req<hai::uptr<yoyo::writer>>::failed("TBD"); });
 }
