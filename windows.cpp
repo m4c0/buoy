1b7a55c40383b2231f50153c5bb238ce6c059f21
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-07 12:11:20 +0100

    windows: pulling "saved games" dir

--- windows.cpp
+++ windows.cpp
@@ -1,4 +1,24 @@
+module;
+#include <shlobj.h>
+
 module buoy;
+
+// https://learn.microsoft.com/en-us/windows/win32/api/shlobj_core/nf-shlobj_core-shgetknownfolderpath
+// FOLDERID_SavedGames
+
+namespace {
+struct deleter {
+  void operator()(PWSTR *p) { CoTaskMemFree(*p); }
+};
+hai::holder<PWSTR, deleter> get_sg_folder() {
+  hai::holder<PWSTR, deleter> res{};
+  if (S_OK != SHGetKnownFolderPath(FOLDERID_SavedGames, KF_FLAG_CREATE, nullptr,
+                                   *res)) {
+    return {};
+  }
+  return res;
+}
+} // namespace
 
 mno::req<hai::uptr<yoyo::reader>> buoy::open_for_reading(jute::view) {
   return {};
