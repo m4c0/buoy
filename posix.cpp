9a72e21bd266faab646b5cd36580c1b65d105cc9
Eduardo Costa <m4c0@users.noreply.github.com>
2024-09-28 20:53:49 +0100

    minor refactoring

--- posix.cpp
+++ posix.cpp
@@ -7,25 +7,23 @@ import yoyo;
 
 extern "C" void buoy_get_dir(char *);
 
-mno::req<yoyo::file_reader> buoy::open_for_reading(jute::view folder,
-                                                   jute::view file) {
+static auto filename(jute::view folder, jute::view file) {
   char buf[10240];
   buoy_get_dir(buf);
 
   auto dir = jute::view::unsafe(buf) + "/" + folder;
   mkdir(dir.cstr().begin(), 0777);
 
-  auto fn = (dir + "/" + file).cstr();
+  return (dir + "/" + file).cstr();
+}
+
+mno::req<yoyo::file_reader> buoy::open_for_reading(jute::view folder,
+                                                   jute::view file) {
+  auto fn = filename(folder, file);
   return yoyo::file_reader::open(fn.data());
 }
 mno::req<yoyo::file_writer> buoy::open_for_writing(jute::view folder,
                                                    jute::view file) {
-  char buf[10240];
-  buoy_get_dir(buf);
-
-  auto dir = jute::view::unsafe(buf) + "/" + folder;
-  mkdir(dir.cstr().begin(), 0777);
-
-  auto fn = (dir + "/" + file).cstr();
+  auto fn = filename(folder, file);
   return yoyo::file_writer::open(fn.data());
 }
