262f0025c5f5779230963234893e854162cefb71
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-08 23:44:24 +0100

    create folder on ios

--- objc.mm
+++ objc.mm
@@ -1,7 +1,11 @@
 @import Foundation;
 
-extern "C" void buoy_get_dir(char *buffer) {
-  NSString *dir = [NSSearchPathForDirectoriesInDomains(
-      NSApplicationSupportDirectory, NSUserDomainMask, YES) firstObject];
+extern "C" void buoy_get_dir(char * buffer) {
+  NSArray * arr = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+  NSString * dir = [arr firstObject];
+  [[NSFileManager defaultManager] createDirectoryAtPath:dir
+                            withIntermediateDirectories:YES
+                                             attributes:nil
+                                                  error:nil];
   strcpy(buffer, dir.UTF8String);
 }
