bd38c1ab4f933df1678b1e984da2c400ba59693a
Eduardo Costa <m4c0@users.noreply.github.com>
2025-07-05 11:46:16 +0100

    windows: fix build

--- windows.cpp
+++ windows.cpp
@@ -6,12 +6,13 @@
 // FOLDERID_SavedGames
 
 extern "C" void buoy_get_dir(char * buf, unsigned buf_sz) {
-  hay<PWSTR, nullptr, CoTaskMemFree> sg { nullptr };
-  if (S_OK != SHGetKnownFolderPath(FOLDERID_SavedGames, KF_FLAG_CREATE, nullptr, sg)) {
+  PWSTR sg {};
+  if (S_OK != SHGetKnownFolderPath(FOLDERID_SavedGames, KF_FLAG_CREATE, nullptr, &sg)) {
     buf[0] = 0;
     return;
   }
 
   size_t count {};
   wcstombs_s(&count, buf, buf_sz, sg, buf_sz - 1);
+  CoTaskMemFree(sg);
 }
